b3bb1b90c8f4d3ebdf59cda19366eb62e6952d1a
Adella <adellakenn@gmail.com>
2019-06-06 21:50:05 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,51 @@
+=begin
+  Artist
+  #initialize with #name
+    accepts a name for the artist (FAILED - 1)
+  #name=
+    sets the artist name (FAILED - 2)
+  #add_song
+    keeps track of an artist's songs (FAILED - 3)
+  #save
+    adds the artist instance to the @@all class variable (FAILED - 4)
+  .find_or_create_by_name
+    finds or creates an artist by name maintaining uniqueness of objects by name property (FAILED - 5)
+    Creates new instance of Artist if none exist (FAILED - 6)
+  #print_songs
+    lists all of the artist's songs (FAILED - 7)
+=end
+require 'pry'
+
+class Artist 
+  attr_accessor :name, :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all # needed for the #save method 
+    @@all
+  end
+    
+  def self.find_or_create_by_name(name)
+    new_artist = Artist.new(name)
+    self.all.detect {|artist| artist.name == name}  || Artist.new(name)
+  end
+  
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
+    
+    # binding.pry
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,49 @@
+=begin
+  MP3Importer
+  #initialize
+    accepts a file path to parse mp3 files from (FAILED - 5)
+  #files
+    loads all the mp3 files in the path directory (FAILED - 6)
+    normalizes the filename to just the mp3 filename with no path (FAILED - 7)
+  #import
+    imports the files into the library by creating songs from a filename (FAILED - 8)
+=end
+require 'pry'
+
+class MP3Importer
+  attr_accessor :path
+  
+  def initialize(filepath)  #accepts a file path
+    @path = filepath
+  end
+  
+  
+# Dir are directory streams representing directories of the underlying file system; listing directories and content
+  def files
+    @files = Dir.entries(@path)   # .entries returns an array of all of the files in the given directory
+    # binding.pry
+    @files.delete_if {|file| file == "." || file == ".."} 
+    # binding.pry
+  # Deletes every element of self for which block evaluates to true
+  # needed the "." and ".." because of the output had them both and needed to be deleteed
+    
+  end
+  
+=begin
+    files == [
+    "Real Estate - Green Aisles - country.mp3",
+    "Real Estate - It's Real - hip-hop.mp3",
+    "Action Bronson - Larry Csonka - indie.mp3",
+    "Thundercat - For Love I Come - dance.mp3"
+    ]
+=end
+  
+  def import
+# binding.pry
+    self.files.each do |file|
+      Song.new_by_filename(file)
+   end
+  end
+  
+    
+end
